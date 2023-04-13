18a4e92d8877f1cd28c80496da080ec030d8e553
Tetiana-CTI <102041423+Tetiana-CTI@users.noreply.github.com>
2023-04-13 12:47:38 +0200

    Dodaj pliki projektów.

---
+++ pasjans/Card.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pasjans
+{
+    class Card
+    {
+        private int _valueOfCard;
+        private bool _IsSelected;
+        public int Value
+        {
+            get => _valueOfCard;
+            set
+            {
+                _valueOfCard = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                }
+            }
+        }
+        public bool IsSelected
+        {
+            get => _IsSelected;
+            set
+            {
+                _IsSelected = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+                }
+
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
